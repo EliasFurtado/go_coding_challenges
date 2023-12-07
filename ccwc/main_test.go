a8548516bd4518db23f523bad1cfc8d7b14393ac
Elias Furtado <eliasfurtado.dev@gmail.com>
2023-12-07 20:27:28 -0300

    Refactoring unit tests

--- ccwc/main_test.go
+++ ccwc/main_test.go
@@ -2,110 +2,118 @@ package main
 
 import "testing"
 
-func TestShouldCountBytesInAString(t *testing.T) {
-	word := "abcd"
-	bytes, err := CountBytes(word)
-	expected := 4
+func TestCountBytes(t *testing.T) {
+	t.Run("Count bytes in a string", func(t *testing.T) {
+		word := "abcd"
+		bytes, err := CountBytes(word)
+		expected := 4
 
-	if err != nil {
-		t.Errorf("Unexprected Error '%d'", err)
-	}
-	if bytes != expected {
-		t.Errorf("Expected '%d', Given '%d'", expected, bytes)
-	}
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		if bytes != expected {
+			t.Fatalf("Expected %d, got %d", expected, bytes)
+		}
+	})
+
+	t.Run("Count bytes with empty string", func(t *testing.T) {
+		word := ""
+		_, err := CountBytes(word)
+		expected := "input should not be empty"
+
+		if err == nil {
+			t.Fatal("Expected an error, but no error was returned")
+		}
+
+		if err.Error() != expected {
+			t.Fatalf("Expected '%s', got '%s'", expected, err)
+		}
+	})
 }
 
-func TestCountBytesShouldReturnErrorWithEmptyString(t *testing.T) {
-	word := ""
-	_, err := CountBytes(word)
-	expected := "input should not be empty"
+func TestCountLines(t *testing.T) {
+	t.Run("Count lines in a string", func(t *testing.T) {
+		input := "a\nb\nc\nd\n"
+		lines, err := CountLines(input)
+		expected := 4
 
-	if err == nil {
-		t.Errorf("Expected an error, but no error was returned")
-	}
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		if lines != expected {
+			t.Fatalf("Expected %d, got %d", expected, lines)
+		}
+	})
 
-	if err.Error() != expected {
-		t.Errorf("Expected '%s', Given '%s'", expected, err)
-	}
+	t.Run("Count lines with empty string", func(t *testing.T) {
+		word := ""
+		_, err := CountLines(word)
+		expected := "input should not be empty"
+
+		if err == nil {
+			t.Fatal("Expected an error, but no error was returned")
+		}
+
+		if err.Error() != expected {
+			t.Fatalf("Expected '%s', got '%s'", expected, err)
+		}
+	})
 }
 
-func TestShouldCountLinesInAString(t *testing.T) {
-	input := "a\nb\nc\nd\n"
-	bytes, err := CountLines(input)
-	expected := 4
+func TestCountWords(t *testing.T) {
+	t.Run("Count words in a string", func(t *testing.T) {
+		input := "Hello, world!"
+		words, err := CountWords(input)
+		expected := 2
 
-	if err != nil {
-		t.Errorf("Unexprected Error '%d'", err)
-	}
-	if bytes != expected {
-		t.Errorf("Expected '%d', Given '%d'", expected, bytes)
-	}
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		if words != expected {
+			t.Fatalf("Expected %d, got %d", expected, words)
+		}
+	})
+
+	t.Run("Count words with empty string", func(t *testing.T) {
+		word := ""
+		_, err := CountWords(word)
+		expected := "input should not be empty"
+
+		if err == nil {
+			t.Fatal("Expected an error, but no error was returned")
+		}
+
+		if err.Error() != expected {
+			t.Fatalf("Expected '%s', got '%s'", expected, err)
+		}
+	})
 }
 
-func TestCountLinesShouldReturnErroWithEmptyString(t *testing.T) {
-	word := ""
-	_, err := CountLines(word)
-	expected := "input should not be empty"
+func TestCountChars(t *testing.T) {
+	t.Run("Count chars in a string", func(t *testing.T) {
+		input := "Hello, 世界!"
+		chars, err := CountChars(input)
+		expected := 10
 
-	if err == nil {
-		t.Errorf("Expected an error, but no error was returned")
-	}
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+		if chars != expected {
+			t.Fatalf("Expected %d, got %d", expected, chars)
+		}
+	})
 
-	if err.Error() != expected {
-		t.Errorf("Expected '%s', Given '%s'", expected, err)
-	}
-}
+	t.Run("Count chars with empty string", func(t *testing.T) {
+		word := ""
+		_, err := CountChars(word)
+		expected := "input should not be empty"
 
-func TestShouldCountWordsInAString(t *testing.T) {
-	input := "Hello, world!"
-	bytes, err := CountWords(input)
-	expected := 2
+		if err == nil {
+			t.Fatal("Expected an error, but no error was returned")
+		}
 
-	if err != nil {
-		t.Errorf("Unexprected Error '%d'", err)
-	}
-	if bytes != expected {
-		t.Errorf("Expected '%d', Given '%d'", expected, bytes)
-	}
-}
-
-func TestCountWordsShouldReturnErroWithEmptyString(t *testing.T) {
-	word := ""
-	_, err := CountWords(word)
-	expected := "input should not be empty"
-
-	if err == nil {
-		t.Errorf("Expected an error, but no error was returned")
-	}
-
-	if err.Error() != expected {
-		t.Errorf("Expected '%s', Given '%s'", expected, err)
-	}
-}
-
-func TestShouldCountCharsInAString(t *testing.T) {
-	input := "Hello, 世界!"
-	bytes, err := CountChars(input)
-	expected := 10
-
-	if err != nil {
-		t.Errorf("Unexprected Error '%d'", err)
-	}
-	if bytes != expected {
-		t.Errorf("Expected '%d', Given '%d'", expected, bytes)
-	}
-}
-
-func TestCountCharsShouldReturnErroWithEmptyString(t *testing.T) {
-	word := ""
-	_, err := CountChars(word)
-	expected := "input should not be empty"
-
-	if err == nil {
-		t.Errorf("Expected an error, but no error was returned")
-	}
-
-	if err.Error() != expected {
-		t.Errorf("Expected '%s', Given '%s'", expected, err)
-	}
+		if err.Error() != expected {
+			t.Fatalf("Expected '%s', got '%s'", expected, err)
+		}
+	})
 }
